e458c2b0ad7ec37c346118dab26641386b03ad9f
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-08-26 14:20:09 +0900

    [ADD] authentication - 회원가입 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ insert into authority (authority_name) values ('ROLE_USER');
 insert into authority (authority_name) values ('ROLE_ADMIN');
 
 -- Member 데이터 추가
-insert into member ( member_name, password, email)
-values ('admin', '$2a$10$rzun9GI9GLEOucz05i73IOckCFBR6A/pRw9V4tHMfmzxIyM9HC/ge', 'admin@example.com');
+insert into member ( activated,member_name, password, email)
+values (TRUE,'admin', '$2a$10$tvrp9ZndAgohVjQtxWurZexcViGJvE8Fd6NEdt4qyM3A51XfcIVAG', 'admin@example.com');
 
 -- Member-Authority 관계 데이터 추가
 -- 아래에 있는 MEMBER_ID는 실제 Member 테이블의 member_id 값에 맞게 수정해야 합니다.
